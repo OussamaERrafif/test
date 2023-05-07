22b22fc7ab301addff9fee016c96730baa904d40
OussamaERrafif <110527594+OussamaERrafif@users.noreply.github.com>
2023-05-07 22:23:57 +0100

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
+#include <signal.h>
 
 void handle_sigint(int sig) {
     static int count = 0;
-    printf("Received signal SIGINT (%d)\n", ++count);
-    if (count >= 5) {
-        exit(0);
+    if (sig == SIGINT) {
+        printf("Received SIGINT signal %d\n", ++count);
+        if (count == 5) {
+            exit(0);
+        }
     }
 }
 
 int main() {
-    pid_t pid;
+    int pid;
+    signal(SIGINT, handle_sigint);
     if ((pid = fork()) == 0) {
-        // Child process (P2)
-        signal(SIGINT, handle_sigint);
-        printf("P2 is running with PID %d\n", getpid());
+        printf("Child process P2 with pid %d is running\n", getpid());
         while (1) {
-            pause();
+            // do nothing
         }
     } else {
-        // Parent process (PI)
-        sleep(1);  // Give P2 time to initialize
-        printf("P1 is running with PID %d\n", getpid());
-        printf("Sending SIGINT to P2 (PID %d)\n", pid);
+        printf("Parent process P1 with pid %d is running\n", getpid());
+        sleep(1);
+        printf("Sending SIGINT signal to P2\n");
         kill(pid, SIGINT);
+        while (1) {
+            // do nothing
+        }
     }
     return 0;
 }
