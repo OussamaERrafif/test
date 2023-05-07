e49932feb186290b8b653b5fab07ba9cbb68f114
OussamaERrafif <110527594+OussamaERrafif@users.noreply.github.com>
2023-05-07 22:36:37 +0100

    p2.c

--- p1.c
+++ p1.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
+#include <unistd.h>
 
-void handle_sigint(int sig) {
-    static int count = 0;
-    if (sig == SIGINT) {
-        printf("Received SIGINT signal %d\n", ++count);
-        if (count == 5) {
-            exit(0);
-        }
+int nb_SigInt_recus = 0;
+
+void handler_SIGINT(int sig) {
+    printf("Processus P2 a reçu un signal SIGINT.\n");
+    nb_SigInt_recus++;
+    if (nb_SigInt_recus >= 5) {
+        printf("Processus P2 a reçu %d signaux SIGINT et s'arrête.\n", nb_SigInt_recus);
+        exit(0);
     }
 }
 
 int main() {
-    int pid;
-    signal(SIGINT, handle_sigint);
-    if ((pid = fork()) == 0) {
-        printf("Child process P2 with pid %d is running\n", getpid());
-        while (1) {
-            // do nothing
-        }
-    } else {
-        printf("Parent process P1 with pid %d is running\n", getpid());
+    int nb_SigInt = 5;
+    int pid_P2 = getpid();
+    
+    signal(SIGINT, handler_SIGINT);
+    
+    printf("Processus P2 est en cours d'exécution avec pid %d.\n", pid_P2);
+    
+    printf("Processus P1 envoie un signal SIGINT à P2.\n");
+    kill(pid_P2, SIGINT);
+    
+    printf("Processus P1 envoie %d autres signaux SIGINT à P2.\n", nb_SigInt-1);
+    for (int i = 0; i < nb_SigInt-1; i++) {
         sleep(1);
-        printf("Sending SIGINT signal to P2\n");
-        kill(pid, SIGINT);
-        while (1) {
-            // do nothing
-        }
+        kill(pid_P2, SIGINT);
     }
+    
+    printf("Processus P1 a terminé.\n");
+    
     return 0;
 }
